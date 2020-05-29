26769dbdd836dc394926abb423f69073061393fa
Seth Martin <voiceofsethmartin@gmail.com>
2020-05-29 17:23:43 -0500

    Cleaning up

--- Challenge 4/b64_dec.py
+++
@@ -1,6 +0,0 @@
-from sys import stdin
-import base64
-
-encoded_text = stdin.read().rstrip("\n")
-#encoded_text += "=" * ((4 - len(encoded_text) % 4) % 4)
-print(base64.decodebytes(encoded_text.encode("ascii")).decode("ascii"))
--- Rijndael/test.py
+++
@@ -1,7 +0,0 @@
-from sys import stdin
-
-ciphertext = stdin.read().rstrip("\n")
-print stdin.encoding
-print ciphertext
-print ciphertext.encode("utf-8")
-print type(ciphertext)
